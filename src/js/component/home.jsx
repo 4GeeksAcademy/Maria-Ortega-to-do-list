a111fa32779b3c44c037b5ff6e54876d4c4c8b2f
Maria Camila Ortega <148597026+mcamilaortega@users.noreply.github.com>
2024-01-21 16:55:51 +0000

    working on it

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodos = (todo) => {
+    const task = {
+      label: todo,
+      done: false,
+    };
+    setTodos([...todos, task]);
+    setTodo("");
+  };
+  const removeTodos = (todo) => {
+    const newTodos = todos.filter((task) => task.label !== todo.label);
+    setTodos(newTodos);
+  };
+  return (
+    <div className="text-center">
+      <div className="top">
+        <h1> To-do List </h1>
+        <input
+          value={todo}
+          type="text"
+          placeholder=" add task"
+          onChange={(e) => setTodo(e.target.value)}/>
+        <button onClick={() => addTodos(todo)} className="btn btn-success">
+          Add Task
+        </button>
+      </div>
+      <div className="bottom-list">
+        {todos?.map((task, index) => (
+          <div key={index}>
+            {task.label}
+            <button
+              className="btn btn-danger"
+              onClick={() => removeTodos(task)}>
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
